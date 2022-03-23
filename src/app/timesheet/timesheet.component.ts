00e159bd549278723f678de0e3959d49c04bfa1d
Lijoe <Lijoe@LAPTOP-S8TJ8IDO>
2022-03-24 02:47:41 +0530

    Revert "v 1.6 before bg bootstrap"
    This reverts commit 71d8b31e2e314390207d346ee9cde3cb55aed39f.

--- src/app/timesheet/timesheet.component.ts
+++ src/app/timesheet/timesheet.component.ts
@@ -91,9 +91,6 @@ calCheckUpdated(event)
     
     this.getTimesheet();
     this.timesheet.loginid = 3;
-    // this.timesheet.tsdate = Date.now();
-    console.log(Date.now());
-    
   }
   // onKey(event: any)
   // {
